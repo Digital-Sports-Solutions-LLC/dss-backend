d7f1c1c67f5ab338d7f39220ec3818178c1edc4e
Adam F <59700364+Adam-Flis@users.noreply.github.com>
2024-01-23 12:49:30 -0500

    Pass in data to match summary page

--- dds/match/urls.py
+++ dds/match/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MatchListCreateView, MatchRetrieveUpdateDestroyView, index, match, shotClocker, referee, update, spectator
+from .views import MatchListCreateView, MatchRetrieveUpdateDestroyView, index, match, shotClocker, referee, update, spectator, summary
 
 urlpatterns = [
     path('api/', MatchListCreateView.as_view(), name='match_list_create'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:pk>/SC/<int:num>', shotClocker, name='shotClocker'),
     path('<int:pk>/REF', referee, name='referee'),
     path('<int:pk>/SPECT', spectator, name='spectator'),
-    path('<int:pk>/update', update, name='update')
+    path('<int:pk>/update', update, name='update'),
+    path('<int:pk>/summary', summary, name='summary'),
 ]
