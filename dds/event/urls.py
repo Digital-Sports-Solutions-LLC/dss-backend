72d5eca729999a88d81420170095b7dc5627e04c
Andrew Flis <andrewef2000@gmail.com>
2024-03-25 23:13:41 -0400

    added event page, uploaded dummy event png, added page in events/urls.py

--- dds/event/urls.py
+++ dds/event/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import EventListCreateView, EventRetrieveUpdateDestroyView, addEvent
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('api/', EventListCreateView.as_view(), name='event_list_create'),
     path('api/<int:pk>/', EventRetrieveUpdateDestroyView.as_view(), name='event_retrieve_update_destroy'),
     path('add-event/', addEvent, name='datatest'),
+    path('andrewtest/', TemplateView.as_view(template_name='eventpage.html'), name='EventPage')
 ]
